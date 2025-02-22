b1e28dfd17b4f537260f14aa052cd50064cf8f2f
emsterk05 <emily_kang@college.harvard.edu>
2025-02-22 18:14:39 -0500

    before dance performance

---
+++ tests/testingoutlinkedinapi.py
@@ -0,0 +1,7 @@
+# use linkedin api to get the profile of a user
+
+from linkedin_api import Linkedin
+
+linkedin = Linkedin()
+
+linkedin.get_profile("https://www.linkedin.com/in/joseph-roque-b70000100/")
